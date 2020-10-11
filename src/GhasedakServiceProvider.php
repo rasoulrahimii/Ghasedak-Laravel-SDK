cd1e5499faac2f5ffc5487edcd153f237ab1d946
rahimi <rasoulrahimi93@gmail.com>
2020-10-11 13:03:19 +0330

    initial commit

---
+++ src/GhasedakServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ghasedak\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+use Ghasedak\GhasedakApi;
+
+class GhasedakServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+//        $this->app->register(GhasedakApi::class);
+//        $loader = AliasLoader::getInstance();
+//        $loader->alias('Form', '\Collective\Html\FormFacade');
+
+        $this->app->bind('Laravel' ,function (){
+            return new GhasedakApi();
+        });
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
